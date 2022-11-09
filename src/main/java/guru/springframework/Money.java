75b4e697d06b0f431ceaa17df36a99a2c866b507
Sebastian Kubacki <skubacki@interia.pl>
2022-11-09 18:10:37 +0100

    End section 3

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,48 @@
+package guru.springframework;
+
+
+public class Money implements Expression {
+  protected int amount;
+  protected String currency;
+
+  public Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  protected String currency() {
+    return currency;
+  }
+
+  public static Money dollar(int amount) {
+    return new Money(amount, "USD");
+  }
+
+  public static Money franc(int amount) {
+    return new Money(amount, "CHF");
+  }
+
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return amount == money.amount && currency == money.currency;
+  }
+
+  @Override
+  public String toString() {
+    return "Money [amount=" + amount + ", currency=" + currency + "]";
+  }
+
+  @Override
+  public Expression times(int multiplier) {
+    return new Money(amount * multiplier, this.currency);
+  }
+
+  @Override
+  public Expression plus(Expression addend) {
+    return new Sum(this, addend);
+  }
+
+  public Money reduce(Bank bank, String to) {
+    return new Money(amount / bank.rate(this.currency, to), to);
+  }
+}
